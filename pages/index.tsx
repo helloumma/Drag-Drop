1ffaf47c4ffb2fcd8f1197e94cd70dfca12f7d30
ummagohil <umgo0510@gmail.com>
2023-03-27 15:37:47 +0100

    basic set up

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,13 +11,16 @@ import { useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [isDropped, setIsDropped] = useState(false);
-  const draggableMarkup = <Draggable>Drag me</Draggable>;
+  const containers = ["A", "B", "C"];
+  const [parent, setParent] = useState(null);
+  const draggableMarkup = <Draggable id="draggable">Drag me</Draggable>;
 
-  function handleDragEnd(event: { over: { id: string } }) {
-    if (event.over && event.over.id === "droppable") {
-      setIsDropped(true);
-    }
+  function handleDragEnd(event) {
+    const { over } = event;
+
+    // If the item is dropped over a container, set it as the parent
+    // otherwise reset the parent to `null`
+    setParent(over ? over.id : null);
   }
   return (
     <>
@@ -30,8 +33,15 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.grid}>
           <DndContext onDragEnd={handleDragEnd}>
-            {!isDropped ? draggableMarkup : null}
-            <Droppable>{isDropped ? draggableMarkup : "Drop here"}</Droppable>
+            {parent === null ? draggableMarkup : null}
+
+            {containers.map((id) => (
+              // We updated the Droppable component so it would accept an `id`
+              // prop and pass it to `useDroppable`
+              <Droppable key={id} id={id}>
+                {parent === id ? draggableMarkup : "Drop here"}
+              </Droppable>
+            ))}
           </DndContext>
         </div>
       </main>
