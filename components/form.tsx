df0549b8981d35669cbfe9f67734582dfcdd4bbc
ummagohil <umgo0510@gmail.com>
2023-03-28 14:27:53 +0100

    to do app

--- components/form.tsx
+++ components/form.tsx
@@ -12,22 +12,41 @@ import {
   ButtonGroup,
   Flex,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
-const Form = () => (
-  <Flex>
-    <Input placeholder="Basic usage" />
-    {/*<Menu>
-      <MenuButton as={Button} rightIcon={"V"}>
-        Select Board
-      </MenuButton>
-      <MenuList>
-        <MenuItem>To do</MenuItem>
-        <MenuItem>Currently</MenuItem>
-        <MenuItem>Done</MenuItem>
-      </MenuList>
-</Menu>*/}
-    <Button colorScheme="blue">Add</Button>
-  </Flex>
-);
+const Form = () => {
+  const [input, setInput] = useState<string>("");
+  //const [completedTaskCount, setCompletedTaskCount] = useState(0);
+  const [todoList, setTodoList] = useState<[]>([]);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const onSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (input === "") return alert("please enter a value");
+
+    const newList = todoList.slice();
+    newList.splice(0, 0, { name: input });
+    setTodoList(newList);
+    setInput("");
+  };
+  console.log(todoList);
+  return (
+    <>
+    <Flex>
+      <Input value={input} placeholder="Add item" onChange={onChange} />
+      <Button colorScheme="blue" onClick={onSubmit}>
+        Add
+      </Button>
+     
+    </Flex>
+    {todoList.map((items: any, id: number) => (
+        <p key={id}>{items.name}</p>
+      ))}
+    </>
+  );
+};
 
 export default Form;
