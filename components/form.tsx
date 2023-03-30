977471a649ab8a5329a747b87fec46a9654a18a0
ummagohil <umgo0510@gmail.com>
2023-03-30 17:16:20 +0100

    deploy

--- components/form.tsx
+++ components/form.tsx
@@ -1,35 +1,20 @@
-import {
-  Input,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
-  Button,
-  ButtonGroup,
-  Flex,
-} from "@chakra-ui/react";
+import { Input, Button, Flex } from "@chakra-ui/react";
 import { ChangeEventHandler, MouseEventHandler } from "react";
 
 interface form {
   input: string;
   onChange: ChangeEventHandler<HTMLInputElement> | undefined;
-  onSubmit: MouseEventHandler<HTMLButtonElement> | undefined;
+  onSubmit: MouseEventHandler<HTMLButtonElement> | undefined | any;
 }
 
 const Form = ({ input, onChange, onSubmit }: form) => {
   return (
-    <>
-      <Flex>
-        <Input value={input} placeholder="Add item" onChange={onChange} />
-        <Button colorScheme="blue" type="submit" onClick={onSubmit}>
-          Add
-        </Button>
-      </Flex>
-    </>
+    <Flex>
+      <Input value={input} placeholder="Add item" onChange={onChange} />
+      <Button colorScheme="blue" type="submit" onClick={onSubmit}>
+        Add
+      </Button>
+    </Flex>
   );
 };
 
